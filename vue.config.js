5c553e65c0185a51e839944f1ce3acc1c4e182fc
phil <xiefei_phil@126.com>
2022-10-21 10:38:58 +0800

    初始化yeb前端框架

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+
+let proxyObj = {}
+
+proxyObj['/']={
+  //websocket
+  ws: false,
+  //目标地址
+  target:'http://localhost:8081',
+  //发送请求头host会被设置target
+  changeOrigin: true,
+  //不重写请求地址
+  pathReWrite: {
+    '^/':'/'
+  }
+
+}
+
+module.exports={
+  devServer:{
+    host:'localhost',
+    port: 8080,
+    proxy: proxyObj
+  }
+}
